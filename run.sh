7e18a8fd4cc8cd7abf7dc3e8d553779f304136e3
Zahaib <zahaib.akhtar@gmail.com>
2016-06-20 12:32:04 -0700

    temporary script to run the experiments

---
+++ run.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+> bb-group0-res.txt
+> bb-group1-res.txt
+> bb-group2-res.txt
+> bb-group3-res.txt
+
+k=0;for i in `ls ../../results/convivaData/group0/*.out`; do python simulation.py $i; ((k++)); if [ $k -eq 10 ]; then break; fi; done >> bb-group0-res.txt
+k=0;for i in `ls ../../results/convivaData/group1/*.out`; do python simulation.py $i; ((k++)); if [ $k -eq 10 ]; then break; fi; done >> bb-group1-res.txt
+k=0;for i in `ls ../../results/convivaData/group2/*.out`; do python simulation.py $i; ((k++)); if [ $k -eq 10 ]; then break; fi; done >> bb-group2-res.txt
+k=0;for i in `ls ../../results/convivaData/group3/*.out`; do python simulation.py $i; ((k++)); if [ $k -eq 10 ]; then break; fi; done >> bb-group3-res.txt
+
+
+sed -i 's/QoE: //g; s/ avg. bitrate://g; s/ buf. ratio://g; s/ optimal A://g' bb-group0-res.txt
+sed -i 's/QoE: //g; s/ avg. bitrate://g; s/ buf. ratio://g; s/ optimal A://g' bb-group1-res.txt
+sed -i 's/QoE: //g; s/ avg. bitrate://g; s/ buf. ratio://g; s/ optimal A://g' bb-group2-res.txt
+sed -i 's/QoE: //g; s/ avg. bitrate://g; s/ buf. ratio://g; s/ optimal A://g' bb-group3-res.txt
